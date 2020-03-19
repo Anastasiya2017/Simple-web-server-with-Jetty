f9af1cb96ab738fb212b72eefd6018128b865209
Алюхина Анастасия <anastasiya.belozersk@gmail.com>
2020-03-19 15:33:10 +0300

    -creating 2 servlets -GET, POST request testing: http://localhost:8080/signup          http://localhost:8080/signin

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,19 +1,40 @@
 package main;
 
+import accounts.AccountService;
+import accounts.UserProfile;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import servlets.AllRequestsServlet;
+
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        AccountService accountService = new AccountService();
+
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(allRequestsServlet), "/mirror");
+        //создаем 2 сервлета: 1) для регистрации
+        //                    2) для авторизации
+        // и закладываем эти сервлеты по адресам
+        context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
+        context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
+//        context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/api/v1/users");
+        //добавляем возможность работать со статическими файлами
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
 
         Server server = new Server(8080);
-        server.setHandler(context);
+        server.setHandler(handlers);
 
         server.start();
         java.util.logging.Logger.getGlobal().info("Server started");
---
+++ src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,63 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignUpServlet extends HttpServlet {
+    private final AccountService accountService;
+
+    public SignUpServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String sessionId = req.getSession().getId();
+        UserProfile profile = accountService.getUserBySessionId(sessionId);
+        if (profile == null) {
+            resp.setContentType("text/html;charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        } else {
+            Gson gson = new Gson();
+            String json = gson.toJson(profile);
+            resp.setContentType("text/html;charset=utf-8");
+            resp.getWriter().println(json);
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        if (login == null || password == null) {
+            resp.setContentType("text/html;charset=utf-8");
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (profile != null && profile.getPass().equals(password)) {
+            resp.setContentType("text/html;charset=utf-8");
+            resp.getWriter().println("уже существует!");
+            return;
+        }
+
+        if (profile == null || !profile.getPass().equals(password)) {
+            accountService.addNewUser(new UserProfile(login));
+            accountService.addSession(req.getSession().getId(), profile);
+            Gson gson = new Gson();
+            profile = accountService.getUserByLogin(login);
+            String json = gson.toJson(profile);
+            resp.setContentType("text/html;charset=utf-8");
+            resp.getWriter().println(json);
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }
+    }
+}
